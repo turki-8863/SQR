4f42c010ac24c2f9f2533b1345cc415f23bf8c5a
turki-8863 <tssg8863@gmail.com>
2026-05-09 01:46:22 +0300

    Update SQR.py

--- SQR.py
+++ SQR.py
@@ -70,11 +70,11 @@ def strong_password(password):
         return False
     return True
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "localhost"),
-    "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", ""),
-    "database": os.getenv("DB_NAME", "sqr_db"),
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", "3306")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
     "autocommit": False,
 }
 
